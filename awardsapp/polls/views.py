f14f78145e49c3aa059381e7a7b7cc33e4b1b401
Juan Pablo Marin <jpmarint@gmail.com>
2023-01-31 16:46:38 -0500

    Migrations applied, templates and views created, super user for admin view created

--- awardsapp/polls/views.py
+++ awardsapp/polls/views.py
@@ -4,4 +4,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Estas en la página principal de los Juan Awards")
+
+
+def detail(request, question_id):
+    return HttpResponse(f'Estas viendo la pregunta N°{question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'Estas viendo los resultados de la pregunta N°{question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Estas votando la pregunta N°{question_id}')
